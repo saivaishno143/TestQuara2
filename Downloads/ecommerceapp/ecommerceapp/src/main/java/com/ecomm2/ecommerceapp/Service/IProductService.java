10880a7ef4944798bea3ddf5404b7cd4e787e51f
saivaishno143 <113687905+saivaishno143@users.noreply.github.com>
2025-09-08 08:28:51 +0530

    Delete Downloads/ecommerceapp/ecommerceapp directory

--- Downloads/ecommerceapp/ecommerceapp/src/main/java/com/ecomm2/ecommerceapp/Service/IProductService.java
+++
@@ -1,9 +0,0 @@
-package com.ecomm2.ecommerceapp.Service;
-
-import com.ecomm2.ecommerceapp.Dto.ProductDto;
-
-import java.io.IOException;
-
-public interface IProductService {
-    ProductDto getProductByID(Long id) throws IOException;
-}
